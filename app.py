d16200d583aa3902a2b7eac905234edcef041ad2
Tawsif Rahman Shopnil <shopnil@baiust.edu.bd>
2023-05-08 01:55:47 +0600

    update

--- app.py
+++ app.py
@@ -1,36 +1,22 @@
 from flask import Flask, render_template
 import requests
-import logging
 
 app = Flask(__name__)
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
 
-def get_codeforces_data():
-    url = "https://codeforces.com/api/user.ratedList"
-    response = requests.get(url)
-    users = response.json()["result"]
-
-    results = []
-    for user in users:
-        if "country" in user and user["country"] == "Bangladesh":
-            try:
-                results.append({
-                    "id": user["handle"],
-                    "name": f'{user.get("firstName", "")} {user.get("lastName", "")}'.strip(),
-                    "rating": user["rating"],
-                })
-            except Exception as e:
-                logger.error(f"Error processing user {user}: {e}")
-
-    return results
-
-@app.route("/")
+@app.route('/')
 def ranklist():
-    codeforces_data = get_codeforces_data()
-    codeforces_data.sort(key=lambda x: x["rating"], reverse=True)
+    url_codeforces = "https://codeforces.com/api/user.ratedList"
+    codeforces_response = requests.get(url_codeforces)
+    codeforces_json = codeforces_response.json()
+    codeforces_data = codeforces_json["result"]
 
-    return render_template("ranklist.html", data=codeforces_data, enumerate=enumerate)
+    # Filter for Bangladeshi users and users from BAIUST
+    filtered_data = [
+        user for user in codeforces_data
+        if user.get("country") == "Bangladesh" or "BAIUST" in user.get("organization", "")
+    ]
 
-if __name__ == "__main__":
+    return render_template("ranklist.html", data=filtered_data)
+
+if __name__ == '__main__':
     app.run(debug=True)
